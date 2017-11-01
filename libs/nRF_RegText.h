b1628787c7f7e4c62e2330c0db741ec1ce614281
wassfila <wassim.filali@gmail.com>
2017-11-01 13:50:46 +0100

    Takeover from IoT_Frameworks

---
+++ libs/nRF_RegText.h
@@ -0,0 +1,17 @@
+/** @file nRF_RegText.h
+ *
+ * @author Wassim FILALI
+ *
+ * @compiler IAR STM8
+ *
+ * $Date: 13.12.2015
+ * $Revision:
+ *
+ */
+
+ #include "commonTypes.h"
+
+void nRF_PrintStatus(BYTE status);
+void nRF_PrintConfig(BYTE config);
+
+void nRF_PrintInfo();
